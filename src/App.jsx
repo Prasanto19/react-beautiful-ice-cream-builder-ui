ba6cc1df1176c032d59df12148e6fed6bcb7c5a6
PRASHANTA <prasanto.cou@gmail.com>
2022-05-28 00:01:46 +0900

    Header Component created inside Layout Component.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+import Layout from './components/Layout/Layout'
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Header/>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
