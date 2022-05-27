5ccf8eb8cfcbc9d04b3df12bc64d89c402ef7e04
Prashanta Kumar Dey <prasanto.cou@gmail.com>
2022-05-28 03:19:51 +0900

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
             {/* {Dyanamic year} */}
           </div>
           <div className="textRight">
-            Built with <span className="red">&hearts;</span> by <a href="https://reactjs.org/docs/create-a-new-react-app.html" target="_blank" >Create React App</a>
+            Built with <span className="red">&hearts;</span> by <a href="https://reactjs.org/docs/create-a-new-react-app.html"  >Create React App</a>
           </div>
         </div>
       </footer>
